efdab7a3047bcce5af6736aa5edca20c4df3b56e
luigiecaloUnac <luisg.castro@unac.edu.co>
2021-08-31 14:34:57 -0500

    opncion b,c,d,e  finalizadas

--- taller2/index.py
+++ taller2/index.py
@@ -4,9 +4,8 @@ TALLER #2 - CONDICIONALES, ITERACIONES Y FUNCIONES
 Integrantes
 > Sindy Paola Devoz Perez
 > Freddy Manuel Barrios Del rio
-
+> Luis Guillermo Castro
 Ejecutar programa
-
 a. que imprima todos los números pares entre dos números que ingrese el 
    usuario. Valide que los valores ingresados sean numéricos.
 b. que reciba un número n e imprima los primeros n números triangulares 
@@ -25,13 +24,10 @@ d. que mediante funciones, convierte una cantidad ingresada en pesos colombianos
    a dólares americanos, euros y bitcoins. 
 e. que, mediante una función, organize de forma ascendente, tres valores numéricos ingresados por el usuario.
 f. Salir
-
 '''
-
 options = ['a', 'b', 'c', 'd', 'e', 'f']
 numberPI = 3.1416
 running = True
-
 def main():
     print(menu)
     program = input('Ingresa la letra correspondiente al programa que quieres ejecutar: ')
@@ -41,13 +37,12 @@ def main():
     print("================================================================")
     return program
 
-
 def continueProgram():
     wantContinue = input('Deseas continuar? Ingresa YES or NO: ')
     return wantContinue == 'YES' or wantContinue == 'yes' or wantContinue == 'y'
         
 def printPairs():
-    print('''que imprima todos los números pares entre dos números que ingrese el 
+    print('''Imprima todos los números pares entre dos números que ingrese el 
     usuario. Valide que los valores ingresados sean numéricos.''')
     pairs = []
     
@@ -57,7 +52,7 @@ def printPairs():
         firstNumber = int(firstNumber)
         secondNumber = int(secondNumber)
         if secondNumber < firstNumber:
-            print('El segundo número debe ser mayor')
+            print('El segundo número debe ser mayor')   
         else:
             evaluatedNumber = firstNumber
             while evaluatedNumber <= secondNumber:
@@ -69,14 +64,107 @@ def printPairs():
         print('Los valores no son númericos')
 
 
+def printTriangular():
+    print(''' que reciba un número n e imprima los primeros n números triangulares 
+   junto con su índice. Los números triangulares se obtienen mediante la suma de los números 
+   naturales desde 1 hasta n''')  
+    firstNumber = input('Ingresa el número a triangular: ')
+    if firstNumber.isnumeric() :
+        firstNumber = int(firstNumber)
+        numberInital= 1
+        numberTriangular=0
+        while numberInital <= firstNumber:
+            numberTriangular=numberTriangular+numberInital
+            print(numberInital,'--->',numberTriangular)
+            numberInital=numberInital+1      
+    else:
+        print('Los valores no son númericos')
+
+def printCalls():
+    print('''Calcule el costo de las llamadas realizadas 
+   en un teléfono móvil y la duración en segundos, recibiendo como entrada la tarifa por segundo, 
+   el numero de comunicaciones realizadas y la duración de cada llamada expresada en horas, minutos y segundos.''')
+    secondRate = input('Ingresa la tarifa por segundos: ')
+    numbersCalls = input('Ingresa numero de llamadas realizadas: ')
+    if secondRate.isnumeric() and numbersCalls.isnumeric():
+        secondRate = int(secondRate)
+        numbersCalls = int(numbersCalls)
+        rate=[]
+        cont=1
+        totalScond=0
+        while cont<=numbersCalls:
+            sconds=0
+            print('Ingrese los daos de la llamada {}'.format(cont))
+            hoursCalls = input('Hora: ')
+            minutesCalls = input('Minutos: ')
+            secondCalls = input('Segundos: ')
+            if hoursCalls.isnumeric() and minutesCalls.isnumeric() and secondCalls.isnumeric():
+                hoursCalls = int(hoursCalls)
+                sconds = (hoursCalls*60)*60
+                minutesCalls = int(minutesCalls)
+                secondCalls = int(secondCalls)
+                if minutesCalls <=60 and secondCalls<=60:
+                    sconds=sconds+(minutesCalls*60)
+                    sconds=sconds+secondCalls                    
+                    totalScond=totalScond+sconds
+                    print('La llamada {} es de {} segundos con un valor de  ${}'.format(cont, sconds,sconds*secondRate))
+                    cont=cont+1
+                else:
+                    print('Los minutos y segundos  solo van hasta 60')
+            else:
+                print('Los valores no son númericos')
+        print("Total de todas las llamdas es de {} segundos con un valor de  ${}".format(totalScond,totalScond*secondRate))
+    else:
+        print('Los valores no son númericos')
+
+def printConverter():
+    print(''' que mediante funciones, convierte una cantidad ingresada en pesos colombianos 
+   a dólares americanos, euros y bitcoins.''') 
+    dolar= 0.00027
+    bitcoin= 5.5e-9
+    euro = 0.00022
+    valorCurrency = input('Ingresa el valor en pesos colombianos: ')
+    if valorCurrency.isnumeric() :
+        valorCurrency = int(valorCurrency)
+        print("El valor de  ${} Pesos colombiandos en:".format(valorCurrency))
+        print("Dolares es de {} USD".format(valorCurrency*dolar))
+        print("Euros es de {} EUR".format(valorCurrency*euro))
+        print("Bitcon es de {} XBT".format(valorCurrency*bitcoin))
+    else:
+        print('Los valores no son númericos')
+
+def printOrder():
+    print('que, mediante una función, organize de forma ascendente, tres valores numéricos ingresados por el usuario.')      
+    numeros=[]    
+    for i in range(0,3):
+        num=input("Ingrese valor del numero {} : ".format(i+1))
+        if num.isnumeric() :
+            num = int(num)
+            numeros.append(num)
+        else:
+            print('Los valores no son númericos')
+    numeros.sort()
+    for n in numeros:
+        print(n)
+
+
+        
+
 def process(program):
     if program == 'a':
         printPairs()
+    if program == 'b':
+        printTriangular()
+    if program == 'c':
+        printCalls()
+    if program == 'd':
+        printConverter()
+    if program == 'e':
+        printOrder()
     elif program == 'f':
         print('Good bye! ')
         global running
         running = False
-
 while running:
     #try:
         selectedProgram = main()
@@ -84,9 +172,3 @@ while running:
             continue
         else:
             break
-    #except:
-    #    print('Por favor, ingresa una opción válida')
-    #    continue
-
-
-
